5363fec6aecbd94bb2b5bb9f8ff5113f09bd2d3a
nelmin <nelmindev@proton.me>
2025-05-25 02:16:42 +0200

    Add ShadowJar plugin and configure fat JAR packaging
    Integrate the ShadowJar plugin to support building a fat JAR for the project. Configure the artifact to include a custom file name and a manifest with the main class. Update version to 1.0.0 and link the build task to generate the fat JAR.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "2.1.20"
     kotlin("plugin.serialization") version "2.1.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "dev.nelmin.kotlin"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -27,6 +30,20 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.withType<ShadowJar> {
+    archiveVersion.set(project.version.toString())
+    archiveFileName.set("MinecraftServerSetup.jar")
+
+    manifest {
+        attributes(mapOf("Main-Class" to "dev.nelmin.kotlin.Main"))
+    }
+}
+
+tasks.named("build") {
+   dependsOn(tasks.shadowJar)
 }
